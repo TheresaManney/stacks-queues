d7198fa51fb5f466f2ee41bc5bc34b23d684fc3d
Theresa Manney <theresa.m.17@gmail.com>
2017-02-26 20:45:08 -0800

    Satck.rb and Queue.rb pass all tests in specs file. Did not have time to look at the extra Job Simulation assignment.

--- lib/Queue.rb
+++ lib/Queue.rb
@@ -1,22 +1,34 @@
 class Queue
   def initialize
+    @store_q = Array.new
   end
 
   def enqueue(element)
+    return @store_q.push(element)
   end
 
   def dequeue
+    # first_element = @store_q.first
+    #
+    # @store_q.delete(@store_q.first)
+    # return first_element
+    return @store_q.shift
+
   end
 
   def front
+    return @store_q.first
   end
 
   def size
+    return @store_q.length
   end
 
   def empty?
+    return @store_q.size == 0
   end
 
   def to_s
+    return @store_q.to_s
   end
 end
--- lib/Stack.rb
+++ lib/Stack.rb
@@ -4,18 +4,39 @@ class Stack
   end
 
   def push(element)
+    # unshift adds an element to the end of an array
+    # can be used like this:
+    # @store.unshift(element)
+    # unshift wont work for the rest of the tests though
+    @store.push(element)
+    # NOTE: this is a .push on an array and does not have anything to do with
+    # the fact that we are working with a stack too
   end
 
   def pop
+    return @store.pop
+
+    # Alternate way to write
+    # last_element = @store.last
+    # @store.delete(@store.last)
+    # return last_element
   end
 
   def top
+    return @store.last
   end
 
   def size
+    return @store.length
   end
 
   def empty?
+
+    return size == 0
+
+    # Other ways to write:
+    # return @store.lengt == 0
+    # return @store.empty?
   end
 
   def to_s
